cebfc2943941e47da636cbb240e8d9fc55ed1b33
Jeremy Quadri <jeremy@quadriconsulting.com>
2026-03-19 19:13:36 +0000

    test: trigger resilient advisory layer

--- vulnerable_code/java/UserController.java
+++ vulnerable_code/java/UserController.java
@@ -1,26 +1,21 @@
-package com.example.demo;
+package com.devsecure.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/user")
-    public List<Map<String, Object>> getUser(@RequestParam String username) {
-        // VULNERABLE: Raw string concatenation in SQL query
-        // CWE-89: Improper Neutralization of Special Elements used in an SQL Command
-        // An attacker can supply username=admin' OR '1'='1'-- to bypass authentication
-        String sql = "SELECT id, username, email FROM users WHERE username = '" + username + "'";
-
+    public List<Map<String, Object>> getUser(@RequestParam String id) {
+        // VULNERABLE: Direct string concatenation in Spring JdbcTemplate
+        String sql = "SELECT * FROM users WHERE id = '" + id + "'";
         return jdbcTemplate.queryForList(sql);
     }
 }
