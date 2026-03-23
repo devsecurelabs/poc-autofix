60597c7de81a25072f8d0b148483507e5755614a
Jeremy Quadri <jeremy@quadriconsulting.com>
2026-03-23 02:57:59 +0000

    sql ing file updaten

--- vulnerable_code/sample10.py
+++ vulnerable_code/sample10.py
@@ -8,6 +8,6 @@ def get_user():
     user_id = request.args.get("id")
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    # High-severity SQLi
+    # High-severity SQLi   
     cursor.execute("SELECT * FROM users WHERE id = " + user_id)
     return str(cursor.fetchone())
