8a10d5e8ca6d2fda0f3505089d14798c6cf32361
Jeremy Quadri <jeremy@quadriconsulting.com>
2026-03-23 02:39:18 +0000

    sql ing file updaten

---
+++ vulnerable_code/sample10.py
@@ -0,0 +1,13 @@
+import sqlite3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/user")
+def get_user():
+    user_id = request.args.get("id")
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    # High-severity SQLi
+    cursor.execute("SELECT * FROM users WHERE id = " + user_id)
+    return str(cursor.fetchone())
